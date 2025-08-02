392ea361567ea76199bcee2b97f62e24d7e69560
Jacob Hobbs <JacobRayHobbs@gmail.com>
2025-08-02 12:04:23 -0500

    refactor(com.jacobhobbs.main): This commit makes controller and pogo sub-packages of main

--- src/main/java/com/jacobhobbs/main/SpringBootSandboxApplication.java
+++ src/main/java/com/jacobhobbs/main/SpringBootSandboxApplication.java
@@ -1,13 +1,22 @@
-package com.jacobhobbs;
+package com.jacobhobbs.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+//
 @SpringBootApplication
 public class SpringBootSandboxApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootSandboxApplication.class, args);
+
+	}
+
+	@GetMapping
+	public String helloWorld() {
+		return "Hello World Spring Boot";
 	}
 
 }
