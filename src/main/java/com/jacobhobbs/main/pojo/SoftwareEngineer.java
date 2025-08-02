0b92dae57ecf320cd178c313f4a9ccfee9407151
Jacob Hobbs <JacobRayHobbs@gmail.com>
2025-08-02 15:20:47 -0500

    feat: This commit adds skeleton of basic CRUD API

--- src/main/java/com/jacobhobbs/main/pojo/SoftwareEngineer.java
+++ src/main/java/com/jacobhobbs/main/pojo/SoftwareEngineer.java
@@ -1,9 +1,18 @@
 package com.jacobhobbs.main.pojo;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+
 import java.util.Objects;
 
+@Entity
 public class SoftwareEngineer {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
     private String techStack;
---
+++ src/main/java/com/jacobhobbs/main/repository/SoftwareEngineerRepository.java
@@ -0,0 +1,8 @@
+package com.jacobhobbs.main.repository;
+
+import com.jacobhobbs.main.pojo.SoftwareEngineer;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SoftwareEngineerRepository extends JpaRepository<SoftwareEngineer, Integer> {
+
+}
